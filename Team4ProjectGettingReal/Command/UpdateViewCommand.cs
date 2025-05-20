d04a44d65b61ac23fe49764fe526fdf9a1270344
RalleWhite <55944864+RalleWhite@users.noreply.github.com>
2025-05-20 12:46:24 +0200

    missing files for last commit

---
+++ Team4ProjectGettingReal/Command/UpdateViewCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Team4ProjectGettingReal.ViewModel;
+
+namespace Team4ProjectGettingReal.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainWindowViewModel viewModel;
+
+        public UpdateViewCommand(MainWindowViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Home")
+            {
+                Trace.WriteLine("Home");
+                viewModel.SelectedViewModel = new HomeViewModel();
+            }
+            else if (parameter.ToString() == "Inventory")
+            {
+                Trace.WriteLine("Inventory");
+                viewModel.SelectedViewModel = new InventoryViewModel();
+            }
+        }
+    }
+}
---
+++ Team4ProjectGettingReal/ViewModel/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Team4ProjectGettingReal.Commands;
+using Team4ProjectGettingReal.MVVM;
+
+namespace Team4ProjectGettingReal.ViewModel
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public ICommand UpdateViewCommand { get; set; }
+
+    }
+}
